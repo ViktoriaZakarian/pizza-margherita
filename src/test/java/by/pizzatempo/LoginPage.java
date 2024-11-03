010874532f66fa22e3bdbb7e25e801eddf0ab416
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-03 19:46:29 +0300

    refactor LoginPage

--- src/test/java/by/pizzatempo/LoginPage.java
+++ src/test/java/by/pizzatempo/LoginPage.java
@@ -1,4 +1,36 @@
 package by.pizzatempo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+    private WebDriver webDriver;
+
+    public LoginPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public LoginPage clickButtonPizza() {
+        webDriver.findElement(By.xpath(LoginXpath.BUTTON_PIZZA_XPATH)).click();
+        return this;
+    }
+
+    public LoginPage clickButtonToOrder() {
+        webDriver.findElement(By.xpath(LoginXpath.BUTTON_TO_ORDER_XPATH)).click();
+        return this;
+    }
+
+    public LoginPage clickButtonChooseSize() {
+        webDriver.findElement(By.xpath(LoginXpath.BUTTON_CHOOSE_SIZE_XPATH)).click();
+        return this;
+    }
+
+    public LoginPage clickButtonBasket() {
+        webDriver.findElement(By.xpath(LoginXpath.BUTTON_BASKET_XPATH)).click();
+        return this;
+    }
+
+    public String getTextPizzaMargherita() {
+        return webDriver.findElement(By.xpath(LoginXpath.TEXT_PIZZA_MARGHERITA_XPATH)).getText();
+    }
 }
