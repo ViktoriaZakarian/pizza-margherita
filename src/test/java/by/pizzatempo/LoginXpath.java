2ec89a087f2be008f89398f056c624e33dc75c2f
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-04 00:06:46 +0300

    refactor LoginXpath add BUTTON_DRINKS_XPATH, BUTTON_TO_ORDER_FANTA_XPAT, TEXT_FANTA_XPATH

--- src/test/java/by/pizzatempo/LoginXpath.java
+++ src/test/java/by/pizzatempo/LoginXpath.java
@@ -3,8 +3,12 @@ package by.pizzatempo;
 public class LoginXpath {
 
     public static final String BUTTON_PIZZA_XPATH = "//span[text()='Пицца']";
-    public static final String BUTTON_TO_ORDER_XPATH = "//span[text()='Заказ'][30]";
-    public static final String BUTTON_CHOOSE_SIZE_XPATH = "//div[text()='31'][30]";
-    public static final String BUTTON_BASKET_XPATH = "//div[@class='basket-top']";
-    public static final String TEXT_PIZZA_MARGHERITA_XPATH = "//div[@class='cart-product-name title'][2]";
+    public static final String BUTTON_TO_ORDER_XPATH = "//div[@id='1']//*//span[text()='Заказ']";
+    public static final String BUTTON_CHOOSE_SIZE_XPATH = "//a[@rel='Пицца \"Маргарита\"']//..//..//*//div[text()='31']";
+    public static final String BUTTON_TO_ORDER_2_XPATH = "//a[@rel='Пицца \"Маргарита\"']//..//..//*//div[@class='order-btn']";
+    public static final String BUTTON_BASKET_XPATH = "//div[@class='basket-top']//a[@href='https://www.pizzatempo.by/menu/order/']";
+    public static final String TEXT_PIZZA_MARGHERITA_XPATH = "/html/body/div[1]/div[5]/div[2]/div[2]/div[1]/div[1]/table/tbody/tr[1]/td[2]/div[1]";
+    public static final String BUTTON_DRINKS_XPATH = "//a[@href='https://www.pizzatempo.by/menu/napitki.html']";
+    public static final String BUTTON_TO_ORDER_FANTA_XPATH = "//button[@rel='6010']";
+    public static final String TEXT_FANTA_XPATH = "/html/body/div[1]/div[5]/div[2]/div[2]/div[1]/div[1]/table/tbody/tr[2]/td[2]/div[1]";
 }
