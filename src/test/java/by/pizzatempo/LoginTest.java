de37564abc7cfa3e009795af269fd02670874396
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-04 00:08:57 +0300

    refactor LoginTest add test2

--- src/test/java/by/pizzatempo/LoginTest.java
+++ src/test/java/by/pizzatempo/LoginTest.java
@@ -26,4 +26,19 @@ public class LoginTest extends BaseTest {
 
         Assert.assertEquals(loginPage.getTextPizzaMargherita(), LoginMessage.TEXT_PIZZA_MARGHERITA);
     }
+
+    @Test(testName = "Добавить пиццу \"Маргариту\" любого размера в корзирну, добавить любой напиток в корзину, " +
+            "перейти в корзину, проверить заказ")
+    public void test2() {
+        loginPage
+                .clickButtonToOrder()
+                .clickButtonChooseSize()
+                .clickButtonToOrder2()
+                .clickButtonDrinks()
+                .clickButtonToOrderFanta()
+                .clickButtonBasket();
+
+        Assert.assertEquals(loginPage.getTextPizzaMargherita(), LoginMessage.TEXT_PIZZA_MARGHERITA);
+        Assert.assertEquals(loginPage.getTextFanta(), LoginMessage.TEXT_FANTA);
+    }
 }
