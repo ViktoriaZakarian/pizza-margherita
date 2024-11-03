3d009b1ef5383b3db1c9bca2353fad068b8283ef
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-04 00:07:13 +0300

    refactor LoginMessage add TEXT_FANTA

--- src/test/java/by/pizzatempo/LoginMessage.java
+++ src/test/java/by/pizzatempo/LoginMessage.java
@@ -2,5 +2,6 @@ package by.pizzatempo;
 
 public class LoginMessage {
 
-    public static final String TEXT_PIZZA_MARGHERITA = "Пицца \"Маргарита\"";
+    public static final String TEXT_PIZZA_MARGHERITA = "Пицца \"Маргарита\" (31см)";
+    public static final String TEXT_FANTA = "Фанта апельсин 1л";
 }
