4c7e3d5ac247970d64ee339714cdc88dedad7fca
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-03 19:46:00 +0300

    add LoginXpath

---
+++ src/test/java/by/pizzatempo/LoginXpath.java
@@ -0,0 +1,10 @@
+package by.pizzatempo;
+
+public class LoginXpath {
+
+    public static final String BUTTON_PIZZA_XPATH = "//span[text()='Пицца']";
+    public static final String BUTTON_TO_ORDER_XPATH = "//span[text()='Заказ'][30]";
+    public static final String BUTTON_CHOOSE_SIZE_XPATH = "//div[text()='31'][30]";
+    public static final String BUTTON_BASKET_XPATH = "//div[@class='basket-top']";
+    public static final String TEXT_PIZZA_MARGHERITA_XPATH = "//div[@class='cart-product-name title'][2]";
+}
