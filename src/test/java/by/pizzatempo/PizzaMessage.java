abba28cb4e29c75e3206d48bccaf2ab16445bf14
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-05 01:43:13 +0300

    renamed LoginMessage

--- src/test/java/by/pizzatempo/PizzaMessage.java
+++ src/test/java/by/pizzatempo/PizzaMessage.java
@@ -1,6 +1,6 @@
 package by.pizzatempo;
 
-public class LoginMessage {
+public class PizzaMessage {
 
     public static final String TEXT_PIZZA_MARGHERITA = "Пицца \"Маргарита\" (31см)";
     public static final String TEXT_FANTA = "Фанта апельсин 1л";
